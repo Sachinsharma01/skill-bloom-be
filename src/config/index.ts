e37a24d068fcbcef10d1e33555f6ebde4d80ab3f
Sachin Sharma <sachin2252sharma@gmail.com>
2025-04-19 01:29:02 +0530

    more ogs

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,8 @@ import { config } from 'dotenv';
 const envFile = `.env.${process.env.NODE_ENV || 'development'}`;
 config({ path: envFile });
 
+console.log("ENV File", envFile);
+
 export const {
     PORT,
     NODE_ENV,
