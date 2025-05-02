e0a185c369055f4dc1ca98ab07134dcce741cf18
Sachin Sharma <sachin2252sharma@gmail.com>
2025-05-03 00:44:04 +0530

    refactor error handeling

--- src/utils/error-handler.ts
+++ src/utils/error-handler.ts
@@ -2,13 +2,14 @@ import { Request, Response, NextFunction } from 'express';
 import { CustomError } from './custom-error';
 
 export const errorHandler = (
-    err: Error | CustomError,
+    err: Error | CustomError | { error: any, message: string },
     req: Request,
     res: Response,
     next: NextFunction,
 ) => {
     const statusCode = err instanceof CustomError ? err.statusCode : 500;
     const message = err.message || 'Internal Server Error';
+    const error = 'error' in err ? err.error : undefined;
 
-    res.status(statusCode).json({ error: message });
+    res.status(statusCode).json({ errorMessage: message, error });
 };
